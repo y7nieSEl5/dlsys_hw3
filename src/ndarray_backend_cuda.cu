5f21e014a1bb95512d3f1cd5f5238f147d65abfc
Yifei Lu <yifeil0725@gmail.com>
2026-07-31 15:28:57 +0800

    reverse order

--- src/ndarray_backend_cuda.cu
+++ src/ndarray_backend_cuda.cu
@@ -101,9 +101,9 @@ __global__ void CompactKernel(const scalar_t* a, scalar_t* out, size_t size, Cud
   if (gid < size) {
     size_t idx = offset;
     size_t tmp = gid;
-    for (size_t i = 0; i < shape.size; i++) {
-      size_t dim_idx = tmp / shape.data[i];
-      tmp = tmp % shape.data[i];
+    for (size_t i = shape.size - 1; i >= 0; i--) {
+      size_t dim_idx = tmp % shape.data[i];
+      tmp = tmp / shape.data[i];
       idx += dim_idx * strides.data[i];
     }
     out[gid] = a[idx];
@@ -142,9 +142,9 @@ __global__ void EwiseSetitemKernel(const scalar_t* a, scalar_t* out, size_t size
   if (gid < size) {
     size_t idx = offset;
     size_t tmp = gid;
-    for (size_t i = 0; i < shape.size; i++) {
-      size_t dim_idx = tmp / shape.data[i];
-      tmp = tmp % shape.data[i];
+    for (size_t i = shape.size - 1; i >= 0; i--) {
+      size_t dim_idx = tmp % shape.data[i];
+      tmp = tmp / shape.data[i];
       idx += dim_idx * strides.data[i];
     }
     out[idx] = a[gid];
@@ -181,9 +181,9 @@ __global__ void ScalarSetitemKernel(size_t size, scalar_t val, scalar_t* out, Cu
   if (gid < size) {
     size_t idx = offset;
     size_t tmp = gid;
-    for (size_t i = 0; i < shape.size; i++) {
-      size_t dim_idx = tmp / shape.data[i];
-      tmp = tmp % shape.data[i];
+    for (size_t i = shape.size - 1; i >= 0; i--) {
+      size_t dim_idx = tmp % shape.data[i];
+      tmp = tmp / shape.data[i];
       idx += dim_idx * strides.data[i];
     }
     out[idx] = val;
